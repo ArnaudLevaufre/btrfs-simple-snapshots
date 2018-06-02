241e2a60780137d4d32ed5875305802e3fc0d6ca
Arnaud Levaufre <arnaud@levaufre.name>
2018-06-02 19:56:07 +0200

    Release version 0.1.1
    This release fixes the pypi packagin

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name="btrfs-simple-snapshots",
-    version="0.1.0",
+    version="0.1.1",
     description="Snapshot you btrfs subvolumes and apply a retention policy",
     long_description=long_description,
     url="https://github.com/ArnaudLevaufre/btrfs-simple-snapshots",
